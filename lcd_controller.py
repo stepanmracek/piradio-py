0e13659a0ac391cfdb25ae0d7b58fb85d2604851
Stepan Mracek <stepan.mracek@gmail.com>
2018-05-16 12:35:10 +0000

    lcd: mutex fix, nicer volume control

--- lcd_controller.py
+++ lcd_controller.py
@@ -12,6 +12,7 @@ from os import system
 
 stations = []
 status = None
+volume = 0
 with open("api-key.txt", "rb") as api_key_file:
     api_key = api_key_file.read()
 headers = {"Api-Key": base64.b64encode(api_key)}
@@ -45,16 +46,14 @@ def stop():
         print(e)
 
 
-def volume_set(shift):
+def volume_set(current, shift):
     try:
-        r = requests.get("http://127.0.0.1:3000/volume", headers=headers)
-        if r.status_code == 200:
-            v = r.json() + shift
-            if v < 0:
-                v = 0
-            if v > 100:
-                v = 100
-            requests.post("http://127.0.0.1:3000/volume", json={"volume": v}, headers=headers)
+        v = current + shift
+        if v < 0:
+            v = 0
+        if v > 100:
+            v = 100
+        requests.post("http://127.0.0.1:3000/volume", json={"volume": v}, headers=headers)
     except BaseException as e:
         print(e)
 
@@ -78,10 +77,15 @@ def get_menu_items():
     return items
 
 
+def generate_volume_bar():
+    v = int(volume / 10)
+    bar = "\x01" + "".join(["\x02" for i in range(v)]) + "".join(["\x03" for i in range(10 - v)]) + "\x04"
+    return [{"name": bar, "action": None}]
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("radio/stations")
-    client.subscribe("radio/status")
+    client.subscribe("radio/#")
 
 
 def on_message(client, userdata, msg):
@@ -96,13 +100,23 @@ def on_message(client, userdata, msg):
     elif msg.topic == "radio/status":
         global status
         status = payload
-    stationsContent.update_items(get_menu_items())
+    elif msg.topic == "radio/volume":
+        global volume
+        volume = int(payload)
+        volume_content.update_items(generate_volume_bar())
+        volume_content.begin_reached_action = partial(volume_set, volume, 10)
+        volume_content.end_reached_action = partial(volume_set, volume, -10)
+    stations_content.update_items(get_menu_items())
     menuController.display()
 
 
 lcd = LCD.Adafruit_CharLCDPlate()
 lcd.set_backlight(False)
 lcd.clear()
+lcd.create_char(1, [1, 2, 2, 2, 2, 2, 1, 0])
+lcd.create_char(2, [31, 21, 10, 21, 10, 21, 31, 0])
+lcd.create_char(3, [31, 0, 0, 0, 0, 0, 31, 0])
+lcd.create_char(4, [16, 8, 8, 8, 8, 8, 16, 0])
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -112,18 +126,19 @@ client.connect("127.0.0.1")
 reader = ButtonReader(lcd)
 reader.start()
 
-stationsContent = MenuContent([])
+stations_content = MenuContent("Stations", [])
 
-systemContent = MenuContent([
-    {"name": "Volume +", "action": partial(volume_set, 10)},
-    {"name": "Volume -", "action": partial(volume_set, -10)},
+system_content = MenuContent("System", [
     {"name": get_ip(), "action": None},
     {"name": "Power off", "action": partial(system, "sudo poweroff")},
 ])
 
-rootContent = MenuContent([
-    {"name": "Stations", "action": lambda: menuController.push_content(stationsContent)},
-    {"name": "System", "action": lambda: menuController.push_content(systemContent)}
+volume_content = MenuContent("Volume", [])
+
+rootContent = MenuContent("PiRadio", [
+    {"name": "Stations", "action": lambda: menuController.push_content(stations_content)},
+    {"name": "Volume", "action": lambda: menuController.push_content(volume_content)},
+    {"name": "System", "action": lambda: menuController.push_content(system_content)}
 ])
 menuController = MenuController(reader, lcd, rootContent)
 
--- lcd_menu.py
+++ lcd_menu.py
@@ -1,13 +1,15 @@
 from button_reader import ButtonReader, PRESSED
 import Adafruit_CharLCD as LCD
+from threading import Lock
 
 
 class MenuController(object):
-    def __init__(self, button_reader, lcd, initial_content):
+    def __init__(self, button_reader: ButtonReader, lcd, initial_content):
         super(MenuController, self).__init__()
         self.button_reader = button_reader
         self.lcd = lcd
         self.contents = [initial_content]
+        self.lock = Lock()
         self.display()
         self.button_reader.clear_all_actions()
         self.button_reader.add_action(LCD.DOWN, PRESSED, lambda b, a: self._on_select(+1))
@@ -21,13 +23,15 @@ class MenuController(object):
         self.display()
 
     def display(self):
-        self.lcd.clear()
-        if len(self.contents) == 0:
-            self.lcd.message("<no content>")
-        elif self.contents[-1].selected_index > len(self.contents[-1].items) - 1:
-            self.lcd.message("<no item>")
-        else:
-            self.lcd.message(self.contents[-1].items[self.contents[-1].selected_index]["name"])
+        with self.lock:
+            self.lcd.clear()
+            if len(self.contents) == 0:
+                self.lcd.message("<no content>")
+            elif self.contents[-1].selected_index > len(self.contents[-1].items) - 1:
+                self.lcd.message(self.contents[-1].title + "\n<no item>")
+            else:
+                c = self.contents[-1]
+                self.lcd.message(c.title + "\n" + c.items[c.selected_index]["name"])
 
     def _on_select(self, shift):
         if len(self.contents) == 0:
@@ -35,7 +39,14 @@ class MenuController(object):
         content = self.contents[-1]
         count = len(content.items)
         if count > 0:
-            content.selected_index = (content.selected_index + shift) % count
+            new_index = content.selected_index + shift
+            if new_index < 0 and content.begin_reached_action is not None:
+                content.begin_reached_action()
+                return
+            elif new_index >= count and content.end_reached_action is not None:
+                content.end_reached_action()
+                return
+            content.selected_index = (new_index) % count
             self.display()
 
     def _on_back(self):
@@ -56,12 +67,17 @@ class MenuController(object):
 
 
 class MenuContent(object):
-    def __init__(self, items):
+    def __init__(self, title: str, items: list):
         super(MenuContent, self).__init__()
+        self.title = title
         self.selected_index = 0
         self.items = items
+        self.begin_reached_action = None
+        self.end_reached_action = None
+        self.lock = Lock()
 
     def update_items(self, items):
-        self.items = items
-        if self.selected_index > len(self.items) - 1:
-            self.selected_index = 0
+        with self.lock:
+            self.items = items
+            if self.selected_index > len(self.items) - 1:
+                self.selected_index = 0
